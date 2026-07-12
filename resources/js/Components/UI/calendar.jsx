10d939dd767fbefd09fbca3090d4f068d2339f6b
rizkychandra <rizkychandra2204@gmail.com>
2026-07-13 03:51:11 +0700

    feat: implement comprehensive UI component library based on Shadcn/UI standards

--- resources/js/Components/UI/calendar.jsx
+++ resources/js/Components/UI/calendar.jsx
@@ -1,8 +1,10 @@
+"use client"
+
 import * as React from "react"
 import { DayPicker, getDefaultClassNames } from "react-day-picker";
 
 import { cn } from "@/lib/utils"
-import { Button, buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/Components/UI/button"
 import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon } from "lucide-react"
 
 function Calendar({
@@ -65,7 +67,7 @@ function Calendar({
         caption_label: cn("font-medium select-none", captionLayout === "label"
           ? "text-sm"
           : "flex items-center gap-1 rounded-(--cell-radius) text-sm [&>svg]:size-3.5 [&>svg]:text-muted-foreground", defaultClassNames.caption_label),
-        table: "w-full border-collapse",
+        month_grid: cn("w-full border-collapse", defaultClassNames.month_grid),
         weekdays: cn("flex", defaultClassNames.weekdays),
         weekday: cn(
           "flex-1 rounded-(--cell-radius) text-[0.8rem] font-normal text-muted-foreground select-none",
